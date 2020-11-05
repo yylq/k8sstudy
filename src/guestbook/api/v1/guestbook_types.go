26b9eb7a4f62d1d6049d35f0e0236911f2d44925
liqiang881 <liqiang26@jd.com>
2020-11-05 14:09:47 +0800

    add status

--- src/guestbook/api/v1/guestbook_types.go
+++ src/guestbook/api/v1/guestbook_types.go
@@ -36,6 +36,7 @@ type GuestbookSpec struct {
 type GuestbookStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	value string `json:"value,omitempty"`
 }
 
 // +kubebuilder:object:root=true
